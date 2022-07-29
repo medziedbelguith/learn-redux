c6ce42b733fdbbaaff948e07bf4d708fdcffd869
medziedbelguith <medziedbelguith@gmail.com>
2022-07-29 13:00:42 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -3,12 +3,43 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux';
+import allReducer from './reducers/index';
+import {Provider} from 'react-redux';
+/* //ACTION
+const increment=()=>{
+  return{
+    type:'INCREMENT'
+  }
+}
+const decrement=()=>{
+  return{
+    type:'DECREMENT'
+  }
+}
+//REDUCER
+const counter=(state=0,action)=>{
+  switch(action.type){
+    case "INCREMENT":
+      return state+1;
+    case "DECREMENT":
+      return state-1;
+  }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+} */
+let myStore=createStore(allReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+//DISPATCH
+
+/* store.subscribe(()=>console.log(store.getState()));
+store.dispatch(increment());
+store.dispatch(decrement());
+store.dispatch(decrement()); */
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <Provider store={myStore}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
